f7a6a2786f7b7bc9d7876140c879e61e64818929
Brian Montana <contact@brianmontana.net>
2018-01-17 06:41:57 -0600

    Ejecting the react setup and converting to Sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import Hero from './components/Hero';
 import Map from './components/Map';
 import { fetchData } from "./utilities/helpers";
